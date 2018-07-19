23bb6b701aba3396658bc2ff9fcfd31ff36b7952
IshJ <ishadij.12@cse.mrt.ac.lk>
2018-07-19 10:05:41 +0530

    initial commit

--- geoserver/geoserver/settings/local.py
+++ geoserver/geoserver/settings/local.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'geodb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '',
         'PORT': '',
     }
--- geoserver/geoserver/urls/local.py
+++ geoserver/geoserver/urls/local.py
@@ -1,26 +1,32 @@
-from django.conf.urls import patterns, include, url
+
+
+from django.conf.urls import  include, url
 
 from django.contrib import admin
 from geoserver import settings
+# from settings.local import MEDIA_ROOT, STATIC_ROOT
+from django.views.static import serve
+
+import semantics.urls as semantic_urls
+import labels.urls as labels_urls
+# from questions.urls import urlpatterns
+import questions.urls
+import semantics.urls
+import labels.urls
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'geoserver.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
+
+urlpatterns = [
     url(r'^semantics/', include('semantics.urls')),
     url(r'^labels/', include('labels.urls')),
     url(r'^questions/', include('questions.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^admin/', admin.site.urls)
+]
 
 if settings.local.DEBUG:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$',
-            'django.views.static.serve',
-            {'document_root': settings.local.MEDIA_ROOT, }),
-        url(r'^static/(?P<path>.*)$',
-            'django.views.static.serve',
-            {'document_root': settings.local.STATIC_ROOT, }),
-    )
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.local.MEDIA_ROOT }),
+        url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.local.STATIC_ROOT }),
+   ]
